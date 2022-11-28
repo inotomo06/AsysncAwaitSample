8a1822f2b4585415bfeb427b111b8325bd14c7f8
Inoue <21cm0106@jec.ac.jp>
2022-11-28 23:46:41 +0900

    Initial Commit

---
+++ AsysncAwaitSample/AsysncAwaitSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsysncAwaitSampleApp.swift
+//  AsysncAwaitSample
+//
+//  Created by cmStudent on 2022/11/28.
+//
+
+import SwiftUI
+
+@main
+struct AsysncAwaitSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
